d94d2fe48996f714c9fa606966ec248295305157
Saveyourmoon <153912927+Saveyourmoon@users.noreply.github.com>
2024-05-07 16:48:04 +0200

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -4,7 +4,7 @@
 // @version      0.4.3
 // @description  try to take over the world!
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
-// @match        https://*.labs.staratlas.com/
+// @match        https://based.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
 // @require      https://raw.githubusercontent.com/Saveyourmoon/Miner/main/anchor-browserified.js
 // @require      https://raw.githubusercontent.com/Saveyourmoon/Miner/main/buffer-browserified.js
